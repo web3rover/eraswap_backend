50bd390fe83cd50036deef893cab0543f70e108b
saikat <saikatchakrabortty2@gmail.com>
2018-11-23 21:20:36 +0530

    Remove the job if its not received after 10 minutes also

--- agenda/index.js
+++ agenda/index.js
@@ -1,6 +1,7 @@
 var Agenda = require('agenda');
 var config = require('../configs/config');
 var txnCont = require('../controllers/transaction');
+var crypto = require('../helpers/cryptos');
 
 const agenda = new Agenda({
   db: {
@@ -18,78 +19,89 @@ const agenda = new Agenda({
 agenda.define('CheckForTxn and Send',(job,done)=>{
     // console.log(job.data);
     const jobData = job.attrs.data;
-    txnCont
-    .verifyTxn(jobData.eraswapSendAddress, jobData.lctxid, jobData.tiMeFrom, jobData.exchangePlatform, jobData.exchFromCurrency, jobData.exchFromCurrencyAmt)
-    .then(data => {
-      if (data.txIdExist && data.status == "ok" && !data.convertedYet) {
-        txnCont.converTdata(jobData.symbol, jobData.lctxid,jobData.exchangePlatform,jobData.exchFromCurrency,jobData.exchToCurrency,jobData.exchFromCurrencyAmt)
-        .then(conversation_data=>{
-          // if(conversation_data.status=="closed"&& conversation_data.cost ==jobData.exchFromCurrencyAmt){
-          //   txnCont
-          // .sendToCustomer(data._id, jobData.userID, jobData.exchangePlatform, jobData.eraswapSendAddress, data.amtToSend, jobData.exchToCurrency)
-          // .then(dataOfSending => {
-          //   if (dataOfSending && dataOfSending.id) {
-          //      job.remove();
-          //      done();
-          //   }
-          // })
-          // .catch(error_sending => {
-          //   return done({
-          //     stack: error_sending,
-          //   });
-          // });    
-          // }
-          done();
-         
-        }).catch(error_converting=>{
-          done(error_converting);
-        });
-      }else if(data.convertedYet==="started"){
-        txnCont.verifyConvertion(jobData.lctxid,jobData.exchangePlatform,jobData.symbol).then(verified=>{
-          if(verified.verified && verified.amtToSend){
-          txnCont
-          .sendToCustomer(data._id, jobData.userID, jobData.exchangePlatform, jobData.eraswapSendAddress, verified.amtToSend, jobData.exchToCurrency)
-          .then(dataOfSending => {
-            if (dataOfSending && dataOfSending.id) {
-               job.remove();
-               done();
-            }
-          })
-          .catch(error_sending => {
-            return done({
-              stack: error_sending,
-            });
-          });
-        }
-        }).catch(error_verfctn=>{
-          done(error_verfctn);
-        });
-      }else if(data.convertedYet==="finished" && data.amtToSend){
+    const failCount = job.attrs.failCount || 0;
+return crypto.getCurrentMarket(jobData.exchangePlatform, jobData.symbol).then(curMar=>{
+  return txnCont
+  .verifyTxn(jobData.eraswapSendAddress, jobData.lctxid, jobData.tiMeFrom, jobData.exchangePlatform, jobData.exchFromCurrency, jobData.exchFromCurrencyAmt)
+  .then(data => {
+    // after 4*2 =10 minutes delete the job 
+    if(failCount > 3 && !data.txIdExist){
+      job.remove();
+      done();
+    }
+    if (data.txIdExist && data.status == "ok" && !data.convertedYet) {
+      txnCont.converTdata(curMar.symbol, jobData.lctxid,jobData.exchangePlatform,jobData.exchFromCurrency,jobData.exchToCurrency,jobData.exchFromCurrencyAmt)
+      .then(conversation_data=>{
+        // if(conversation_data.status=="closed"&& conversation_data.cost ==jobData.exchFromCurrencyAmt){
+        //   txnCont
+        // .sendToCustomer(data._id, jobData.userID, jobData.exchangePlatform, jobData.eraswapSendAddress, data.amtToSend, jobData.exchToCurrency)
+        // .then(dataOfSending => {
+        //   if (dataOfSending && dataOfSending.id) {
+        //      job.remove();
+        //      done();
+        //   }
+        // })
+        // .catch(error_sending => {
+        //   return done({
+        //     stack: error_sending,
+        //   });
+        // });    
+        // }
+        done();
+       
+      }).catch(error_converting=>{
+        done(error_converting);
+      });
+    }else if(data.convertedYet==="started"){
+      txnCont.verifyConvertion(jobData.lctxid,jobData.exchangePlatform,curMar.symbol).then(verified=>{
+        if(verified.verified && verified.amtToSend){
         txnCont
-          .sendToCustomer(data._id, jobData.userID, jobData.exchangePlatform, jobData.eraswapSendAddress, data.amtToSend, jobData.exchToCurrency)
-          .then(dataOfSending => {
-            if (dataOfSending && dataOfSending.id) {
-               job.remove();
-               done();
-            }
-          })
-          .catch(error_sending => {
-            return done({
-              stack: error_sending,
-            });
+        .sendToCustomer(data._id, jobData.userID, jobData.exchangePlatform, jobData.eraswapSendAddress, verified.amtToSend, jobData.exchToCurrency)
+        .then(dataOfSending => {
+          if (dataOfSending && dataOfSending.id) {
+             job.remove();
+             done();
+          }
+        })
+        .catch(error_sending => {
+          return done({
+            stack: error_sending,
           });
-      } else {
-        return done({
-          message: 'Verification failed. No deposit Found.',
         });
       }
-    })
-    .catch(error => {
-      return done({
-        message: error.message||'Verification failed.',
-        error: error,
+      }).catch(error_verfctn=>{
+        done(error_verfctn);
       });
+    }else if(data.convertedYet==="finished" && data.amtToSend){
+      txnCont
+        .sendToCustomer(data._id, jobData.userID, jobData.exchangePlatform, jobData.eraswapSendAddress, data.amtToSend, jobData.exchToCurrency)
+        .then(dataOfSending => {
+          if (dataOfSending && dataOfSending.id) {
+             job.remove();
+             done();
+          }
+        })
+        .catch(error_sending => {
+          return done({
+            stack: error_sending,
+          });
+        });
+    } else {
+      return done({
+        message: 'Verification failed. No deposit Found.',
+      });
+    }
+  })
+  .catch(error => {
+    return done({
+      message: error.message||'Verification failed.',
+      error: error,
     });
+  });
+}).catch(error=>{
+  done(error);
+})
+   
 });
 
 
