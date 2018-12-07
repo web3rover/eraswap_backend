a8095b3d33b12f3ea675afd0b198be2b68c25602
Utkarsh Mehta <utkarsh.mehta678@live.com>
2018-12-07 18:18:10 +0530

    ComputeX integration in wallets

--- models/Transactions.js
+++ models/Transactions.js
@@ -1,99 +1,101 @@
 const mongoose = require('mongoose');
 
 const TxnSchema = new mongoose.Schema({
-userId:{
-    type:mongoose.Schema.Types.ObjectId,
-    ref:'Users',
-    required:true
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Users',
+        required: true
+    },
+    dipositTxnId: {
+        type: String
+    },
+    dipositTxnStatus: {
+        type: String,
+    },
+    refunded: {
+        type: Boolean,
+        default: false
+    },
+    cancelledConvertion: {
+        type: Boolean,
+        default: false
+    },
+    depositNotFpund: {
+        type: Boolean,
+        default: true
+    },
+    orderplacingAmt: {
+        type: String,
+    },
+    side: {
+        type: String,
+    },
+    conversation_fees: {
+        type: Number
+    },
+    convertedYet: {
+        type: String,
+        default: null,
+        enum: [null, "started", "finished"]
+    },
+    amtToSend: {
+        type: Number
+    },
+    orderId: {
+        type: String
+    },
+    convertionTime: {
+        type: Number
+    },
+    witdrawn: {
+        type: Boolean,
+        default: false
+    },
+    exchFromCurrency: {
+        type: String,
+        required: true
+    },
+    exchFromCurrencyAmt: {
+        type: Number,
+        required: true
+    },
+    exchToCurrency: {
+        type: String,
+        required: true
+    },
+    exchToCurrencyRate: {
+        type: Number,
+        required: true
+    },
+    allExchResult: {
+        type: Array
+    },
+    eraswapAcceptAddress: {
+        type: String,
+        require: true
+    },
+    eraswapSendAddress: {
+        type: String,
+        require: true
+    },
+    exchangePlatform: {
+        type: String,
+    },
+    totalExchangeAmout: {
+        type: String,
+        required: true
+    },
+    platformFeePayOpt: {
+        type: String,
+        default: 'EST',
+        enum: ['EST', 'source']
+    },
+    ersToCastTxid: {
+        type: String
+    },
+    fromWallet: { type: Boolean, default: false },
 },
-dipositTxnId:{
-    type:String
-},
-dipositTxnStatus:{
-    type:String,
-},
-refunded:{
-    type:Boolean,
-    default:false
-},
-cancelledConvertion:{
-    type:Boolean,
-    default:false
-},
-depositNotFpund:{
-    type:Boolean,
-    default:true
-},
-orderplacingAmt:{
-    type:String,
-},
-side:{
-    type:String,
-},
-conversation_fees:{
-    type:Number
-},
-convertedYet:{
-    type:String,
-    default:null,
-    enum:[null,"started","finished"]
-},
-amtToSend:{
-    type:Number
-},
-orderId:{
-    type:String
-},
-convertionTime:{
-    type: Number
-},
-witdrawn:{
-    type:Boolean,
-    default:false
-},
-exchFromCurrency:{
-    type:String,
-    required:true
-},
-exchFromCurrencyAmt:{
-    type:Number,
-    required:true
-},
-exchToCurrency:{
-    type:String,
-    required:true
-},
-exchToCurrencyRate:{
-    type:Number,
-    required:true
-},
-allExchResult:{
-    type:Array
-},
-eraswapAcceptAddress:{
-    type:String,
-    require:true
-},
-eraswapSendAddress:{
-    type:String,
-    require:true
-},
-exchangePlatform:{
-    type:String,
-},
-totalExchangeAmout:{
-    type:String,
-    required:true
-},
-platformFeePayOpt:{
-    type:String,
-    default:'EST',
-    enum:['EST','source']
-},
-ersToCastTxid:{
-    type:String
-}
-},{timestamps:true});
+    { timestamps: true });
 
 const Txn = mongoose.model('Txn', TxnSchema);
 module.exports = Txn;
