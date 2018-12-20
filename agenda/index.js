81cc0b5b0b86d4df2581e9d321d0ccbe9f421172
saikat <saikatchakrabortty2@gmail.com>
2018-12-21 00:41:02 +0530

    RUn agenda every 5minute

--- agenda/index.js
+++ agenda/index.js
@@ -30,7 +30,7 @@ var start = async function () {
 
     await agenda.start();
 
-    await agenda.every('*/1 * * * *', 'fetch coin value');
+    await agenda.every('*/5 * * * *', 'fetch coin value');
 
     console.log("Started agenda");
 
