0ac9d001a59e03227ea6defb06debe625d30cd32
saikat <saikatchakrabortty2@gmail.com>
2019-01-30 19:56:09 +0530

    few more things regarding buy sell confusion in mail

--- routes/p2p.js
+++ routes/p2p.js
@@ -7,7 +7,7 @@ const config = require('../configs/config');
 
 router.post('/add_buy_listing', (req, res, next) => {
   currencyCont
-    .addListing({ show: true, wantsToBuy: true, email: req.user.email, username: req.user.username, userId: req.user._id, ...req.body })
+    .addListing({ show: true, wantsToSell: true, email: req.user.email, username: req.user.username, userId: req.user._id, ...req.body })
     .then(data => {
       return res.json(data);
     })
@@ -43,7 +43,7 @@ router.post('/add_sell_listing', (req, res, next) => {
 
             if (balanceData && Number(balanceData.balance) >= deductableAmount) {
               currencyCont
-                .addListing({ show: true, wantsToSell: true, email: req.user.email, username: req.user.username, userId: req.user._id, ...req.body })
+                .addListing({ show: true, wantsToBuy: true, email: req.user.email, username: req.user.username, userId: req.user._id, ...req.body })
                 .then(data => {
                   return res.json(data);
                 })
