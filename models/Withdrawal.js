db56f61c32d7975531dcadab26283a7d9166feb1
saikat <saikatchakrabortty2@gmail.com>
2019-02-23 15:56:28 +0530

    small modal change

--- models/Withdrawal.js
+++ models/Withdrawal.js
@@ -1,22 +1,21 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
-var WithdrawalSchema = new mongoose.Schema(
-    {
-        type: { type: String, required: true },
-        status: { type: String, required: true },
-        txn: Object,
-        txnHash: String,
-        feeTxnHash: String,
-        feeError: String,
-        error: String,
-        gasDetails: Object,
-        waitFor: { type: mongoose.Schema.Types.ObjectId, ref: 'Withdrawal' },
-        source: String,
-        orderInfo : Object,
-        agreementInfo : Object,
-    }
-);
+var WithdrawalSchema = new mongoose.Schema({
+  type: { type: String, required: true },
+  status: { type: String, required: true },
+  txn: Object,
+  txnHash: String,
+  feeTxnHash: String,
+  feeError: String,
+  error: String,
+  gasDetails: Object,
+  waitFor: { type: mongoose.Schema.Types.ObjectId, ref: 'Withdrawal' },
+  source: String,
+  orderInfo: Object,
+  p2pInfo: Object,
+  agreementInfo: Object,
+});
 
 const Withdrawal = mongoose.model('Withdrawal', WithdrawalSchema);
 module.exports = Withdrawal;
